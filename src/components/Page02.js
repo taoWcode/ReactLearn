0fed14a1313bd7e5db2b61855f470199714514dd
weitao <itophp@itophp.com>
2018-07-19 09:15:41 +0800

    JSX语法

--- src/components/Page02.js
+++ src/components/Page02.js
@@ -7,6 +7,19 @@
 import React from 'react';
 export default class Page02 extends React.Component{
 	render(){
-		return (<div>这是JSX语法部分</div>)
+		return (<div>
+					<h3>这是JSX语法部分</h3>
+					<p>jsx语法中不能使用"if else语句"</p>
+					<p>{1 ? 'True!' : 'False'}</p>
+					<p style={myStyle}>React推荐使用内联样式</p>
+				{/*注释要写在花括号里面*/}
+					<p>JSX允许在模板中插入数组，数组会自动循环所有成员</p>
+					<a>{arr}</a>
+				</div>)
 	}
 }
+const myStyle = {
+	fontSize:100,
+	color:'#ff0000'
+}
+const arr = ["1","2","3","4"];
